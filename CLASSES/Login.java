ec5743d6c1a55f9642dc295713df1550ad8116d8
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-04-26 13:27:20 +0600

    Update Login.java

--- CLASSES/Login.java
+++ CLASSES/Login.java
@@ -3,37 +3,46 @@ import java.awt.*;
 import javax.swing.*;  
 public class Login{  
 
+  JFrame f;
     JLabel L1,email1,pass1;
     JTextField email ;
     JPasswordField pass;
-    
+    JPanel panel;
     Login()  
         {  
-        JFrame f= new JFrame("Panel Example");    
-        JPanel panel=new JPanel();  
+         f= new JFrame("Panel Example");    
+         panel=new JPanel();  
+         
          email1 = new JLabel("EMAIL :");
+         pass1 = new JLabel( "PASSWORD");
+         pass = new JPasswordField();
+         pass1. setBounds(370,230,104,94);
+         pass.setBounds(476,230,240,66);
          email1.setBackground(Color.black);
-         email1.setBounds(360,76,60,40);
+         email1.setBounds(420,130,100,90);
         f.getContentPane().setBackground(Color.black);
        // f.setBackground(Color.blue);
-        panel.setBounds(345,0,400,450);    
+        panel.setBounds(345,0,400,600);    
         panel.setBackground(Color.gray);  
-
+         email = new JTextField();
+         email.setBounds(480,140,170,50);
        // JButton b1=new JButton("Button 1");     
         //b1.setBounds(50,100,80,30);    
        // b1.setBackground(Color.yellow);   
        // JButton b2=new JButton("Button 2");   
       //  b2.setBounds(100,100,80,30);    
      //   b2.setBackground(Color.green);
-     ImageIcon welcome = new ImageIcon("messagif (2).gif");
+     f.add(pass);
+     f.add(pass1);
+     ImageIcon welcome = new ImageIcon("tt.gif");
       L1 = new JLabel(welcome);
-      L1.setBounds(345, 1, 438, 66);
+      L1.setBounds(263, 18, 588,74);
       f.add(L1);
     panel.add(email1); //panel.add(b2);  
-     
+     f.add(email);
         f.add(email1); 
           f.add(panel);  
-                f.setSize(750,450);    
+                f.setSize(750,580);    
                 f.setLayout(null);    
                 f.setVisible(true);    
         }  
